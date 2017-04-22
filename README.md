ebe5257bf5cb508cca9c9b152d392770069f0db1
Luis <josergp94@gmail.com>
2017-04-22 14:22:33 -0300

    Cambios menores en script, añado instrucciones de uso

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# reco_sueldos
+# Reco Sueldos
 Script de recopilacion de sueldos del sector IT
+
+## Uso
+Recibe como parametro la cantidad de resultados aproximada
+```
+python reco_sueldos.py 700
+```
+
+## Dependencias
+La instalacion de las dependecias se realizan con:
+
+```bash
+pip install requests lxml BeautifulSoup
+```
--- reco_sueldos.py
+++ reco_sueldos.py
@@ -1,24 +1,23 @@
-import requests
 from bs4 import BeautifulSoup as Bs
-
-
+import requests
+from sys import argv
 
 def get_sueldos(html):
     c_html = Bs(html, 'lxml')
     divs = c_html.findAll('div',{'class':'sueldo'})
     return map(lambda div: div.find('a').get('href').split('sueldo/')[-1].split('pesos')[0], divs)
 
-def get_totales(cant):
-    arch = open('sueldos.txt', 'w')
+def get_totales(max_res, min_sueldo, path_res):
+    arch = open(path_res, 'w')
     print "Scrapeo pag 1"
     cgr = requests.get("http://www.cuantogano.com/sueldos/it-programacion.html")
     sueldos = get_sueldos(cgr.text)
     pag = 1
     totales = []
     for sueldo in sueldos:
-        if (eval(sueldo)>1000):
+        if (eval(sueldo)>min_sueldo):
             totales.append(sueldo)
-    while (len(totales) < cant):
+    while (len(totales) < max_res):
         pag += 1
         print "Scrapeo pag %i" % pag
         cgr = requests.get("http://www.cuantogano.com/sueldos/it-programacion.html/pagina%i"%pag)
@@ -29,3 +28,15 @@ def get_totales(cant):
         arch.write(sueldo)
         arch.write('\n')
     print "FINALIZO"
+
+if (len(argv) == 1):
+    print "Debe ingresar parametros:\n"
+    print "python reco_sueldos.py <cantidad_resultados> [<sueldo_minimo>] [<ruta_resultado>]"
+s_min = 2000
+res_path = "sueldos.txt"
+if (len(argv)>2):
+    s_min = eval(argv[2])
+    if (len(argv)>3):
+        res_path = argv[3]
+
+get_totales(eval(argv[1]), s_min, res_path)
